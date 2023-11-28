da272c4024a706e16f956f77261c7ed2d8f3e775
a84793 <nunofcsaude@gmail.com>
2023-11-28 22:01:44 +0000

    Adicionei carrega_as_listas ao io_ficheiros

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -6,3 +6,10 @@ from faturas import nome_ficheiro_lista_de_faturas
 from veiculos import nome_ficheiro_lista_de_veiculos
 
 
+def carrega_as_listas_dos_ficheiros():
+
+    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
+    lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
+    lista_de_faturas = le_de_ficheiro(nome_ficheiro_lista_de_faturas)
+
+    return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
